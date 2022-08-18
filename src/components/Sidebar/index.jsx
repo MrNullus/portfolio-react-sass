1586bdc44f8739b6b885604bf0bd47eff82ff040
dev-gustavo-henrique <gustavojs417@gmail.com>
2022-08-17 22:13:05 -0300

    add changes

---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,12 @@
+
+
+export const Sidebar = () => {
+
+	
+
+	return (
+		<div>
+					
+		</div>
+	)
+}
---
+++ src/pages/App/index.jsx
@@ -0,0 +1,20 @@
+import { Sidebar } from '../../components/Sidebar';
+import { MainContent } from '../../components/MainContent';
+
+import './app.sass';
+
+export function App() {
+
+
+  return (
+    <div id="portifolio">
+
+      <h1>React + SASS</h1>
+
+      <Sidebar />
+      <MainContent />
+
+    </div>
+  );
+
+}
