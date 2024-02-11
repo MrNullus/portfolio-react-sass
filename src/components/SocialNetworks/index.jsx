c83c9ea3e549e9eaae81b7a2f01aeb83826c7b96
Gustavo H. J. <70487766+MrNullus@users.noreply.github.com>
2024-02-11 01:02:50 +0000

    feat: add social links

--- src/components/SocialNetworks/index.jsx
+++ src/components/SocialNetworks/index.jsx
@@ -3,10 +3,10 @@ import { FaLinkedinIn, FaGithub, FaInstagram, FaTwitter } from 'react-icons/fa';
 import './style.sass';
 
 const listSocialNetworks = [
-	{ name: "linkedin", icon: <FaLinkedinIn /> },
-	{ name: "github", icon: <FaGithub /> },
-	{ name: "instagram", icon: <FaInstagram /> },
-	{ name: "twitter", icon: <FaTwitter /> }
+	{ name: "linkedin", icon: <FaLinkedinIn />, link: 'linkedin.com/dev-punk-da-silva' },
+	{ name: "github", icon: <FaGithub />, link: 'github.com/MrNullus' },
+	{ name: "instagram", icon: <FaInstagram />, link: 'instagram.com/punk.da.silva' },
+	{ name: "twitter", icon: <FaTwitter />, link: 'x.com/_MrNullus' }
 ];
 
 export const SocialNetworks = () => {
@@ -16,7 +16,7 @@ export const SocialNetworks = () => {
 			{listSocialNetworks.map(( network ) => (
 				<a 
 					className="social-btn"
-					href="#" 
+					href={network.link} 
 					id={network.name} 
 					key={network.name}
 					target="_blank">
