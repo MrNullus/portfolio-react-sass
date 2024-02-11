a6df4945cad3420b0b364b968add9d6e4ab37bad
Gustavo H. J. <70487766+MrNullus@users.noreply.github.com>
2024-02-11 00:37:34 +0000

    changes

--- src/components/TechnologiesContainer/index.jsx
+++ src/components/TechnologiesContainer/index.jsx
@@ -4,8 +4,19 @@ import {
 	DiJsBadge,
 	DiMysql,
 	DiMsqlServer,
-	DiReact
+	DiReact,
+	DiLaravel,
+    DiGithub,
+    DiRuby,
+    DiPython,
+    DiFirebase
 } from 'react-icons/di';
+import { FaPhp } from "react-icons/fa";
+import { TbBrandNextjs } from "react-icons/tb";
+import { SiExpress } from "react-icons/si";
+import { FaGitSquare } from "react-icons/fa";
+import { FaFigma } from "react-icons/fa";
+import { SiTailwindcss } from "react-icons/si";
 
 import './styles.sass';
 
@@ -17,6 +28,67 @@ const technologies = [
 	{ id: "mySql", name: "mySql", icon: <DiMysql />, desc: "Intermediate Skills" },
 	{ id: "sqlServer", name: "SQL Server", icon: <DiMsqlServer />, desc: "Basic Skills" },
 	{ id: "react", name: "React", icon: <DiReact />, desc: "Intermediate Skills" },
+	{ id: "php", name: "PHP", icon: <FaPhp />, desc: "Intermediate Skills" },
+	{
+		id: "python",
+		name: "Python",
+		icon: <DiPython />,
+		desc: "Intermediate Skills", // Update description based on your skill level
+	  },
+	  {
+		id: "tailwindcss",
+		name: "Tailwind CSS",
+		icon: <SiTailwindcss />,
+		desc: "Intermediate Skills"
+	  },
+	  {
+		id: "github",
+		name: "GitHub",
+		icon: <DiGithub />,
+		desc: "Advanced Skills",
+	  },
+	  {
+		id: "ruby",
+		name: "Ruby",
+		icon: <DiRuby />,
+		desc: "Intermediate Skills", // Update description based on your skill level
+	  },
+	  {
+		id: "figma",
+		name: "Figma",
+		icon: <FaFigma />,
+		desc: "Intermediate Skills", // Update description based on your skill level
+	  },
+	  {
+		id: "git",
+		name: "Git",
+		icon: <FaGitSquare />,
+		desc: "Advanced Skills",
+	  },
+	  {
+		id: "nextjs",
+		name: "Next.js",
+		icon: <TbBrandNextjs />,
+		desc: "Beging Skills", // Update description based on your skill level
+	  },  
+	  {
+		id: "express",
+		name: "Express",
+		icon: <SiExpress />,
+		desc: "Intermediate Skills", // Update description based on your skill level
+	  },
+	  {
+		id: "firebase",
+		name: "Firebase",
+		icon: <DiFirebase />,
+		desc: "Intermediate Skills", // Update description based on your skill level
+	  },
+	  {
+		id: "laravel",
+		name: "Laravel",
+		icon: <DiLaravel />,
+		desc: "Intermediate Skills", // Update description based on your skill level
+	  }
 ];
 
 export const TechnologiesContainer = () => {
