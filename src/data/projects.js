7c5344a6b309a3bce9c8e930e9dcc913da5863c7
Gustavo H. J. <70487766+MrNullus@users.noreply.github.com>
2024-02-18 21:14:31 +0000

    changes

---
+++ src/data/projects.js
@@ -0,0 +1,49 @@
+const projects = [
+	{
+		title: "Coffee Website ☕",
+		description: "This is a project in which I built a responsive website for a fictional coffee shop using HTML5, CSS3 (Flexbox and Grid) and JavaScript.",
+		link: "https://mrnullus.github.io/coffee-website/",
+	},
+	{
+		title: "Food Website 🍴",
+		description: "This project consists of a restaurant one page, built with HTML5 & CSS3 Layout Flexbox & Responsive & JavaScript",
+		link: "https://mrnullus.github.io/food-website/",
+	},
+    {
+		title: "Recipes Website",
+		description: "Responsive website design based on a simple recipe web site",
+        link: "https://you-simply-recipes.netlify.app/"
+	},
+    {
+		title: "Alurakut",
+		description: "This project consists of a copy's Orkut make in React, NextJS, StyledComponents and using concept of BFF (Backend For Frontend) and consuming API in FrontEnd",
+		link: "https://alurakut-delta-dun.vercel.app/",
+	},
+    {
+		title: "HDCHOST",
+		description: "HDCHost is an landing page for enterprise of host and domain web project",
+		link: "https://hdchost.vercel.app/",
+	},
+    {
+		title: "Doceria Velha Infância",
+		description: "This is a web site for promotional candy shop",
+		link: "https://doceria-velha-infancia.vercel.app/",
+	},
+    {
+		title: "Chief Delas Fotos",
+		description: "This repository was made for a website project for a photographer, built in HTML5 and CSS3",
+		link: "https://mrnullus.github.io/site_photo/",
+	},
+    {
+		title: "Mental Health DB",
+		description: "Mental Health DB is a website to provide information about Mental Health: also dealing with other issues that intertwine with our health. Focused in created project is a implementation of techniques for acessibility",
+		link: "https://mrnullus.github.io/mental-health-db/",
+	},
+    {
+		title: "Shop JVJ",
+		description: "JVJ is a fully responsive ecommerce website, maximum compatiblities in all mobile devices, built using HTML, CSS, and JavaScript.",
+		link: "https://shop-jvj.vercel.app/",
+	},
+];
+
+export { projects };
